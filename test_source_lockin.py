a91f09f04f1147f0b3e2d3ccbc8eb00d2c11a8a8
barlowmj <barlowmj@umich.edu>
2019-06-05 15:48:32 -0400

    update

--- test_source_lockin.py
+++ test_source_lockin.py
@@ -8,24 +8,25 @@ lockin = rm.open_resource('GPIB0::5::INSTR')
 
 source.write('SOUR:WAVE:ABOR')
 
-amp = .0002
+source.write('SOUR:WAVE:FUNC SIN')
+source.write('SOUR:WAVE:FREQ 500')
+source.write('SOUR:WAVE:PMAR:STAT ON')
+source.write('SOUR:WAVE:PMAR:OLIN 1')
+lockin.write('PHAS 0.00')
+lockin.write('OFLT 6')
+time.sleep(1)
+
+amp = .0004
 for i in range(4):
     source.write(f'SOUR:WAVE:AMPL {amp}') # see if this works?
-	source.write('SOUR:WAVE:ARM')
-	source.write('SOUR:WAVE:INIT')
-    time.sleep(60) # mess with time constant to see if this can be reduced
-	x = lockin.query_ascii_values('OUTP? 1')
-	y = lockin.query_ascii_values('OUTP? 2')
-	volt = sqrt(x[0]**2 + y[0]**2)
-	res = volt / amp
-	print(res)
+    source.write('SOUR:WAVE:ARM')
+    source.write('SOUR:WAVE:INIT')
+    time.sleep(0.1) # mess with time constant to see if this can be reduced
+    vals = lockin.query_ascii_values('SNAP? 1,2')
+    volt = sqrt(vals[0]**2 + vals[1]**2)
+    res = volt / amp
+    print(res)
     source.write('SOUR:WAVE:ABOR') # does it need to be aborted each time or would changing the amplitude be sufficient? test this
-	amp += 0.0002
-    time.sleep(1.5) # see how short of a sleep period works
-	
-
-
-
-
-
+    amp += 0.0002
+    time.sleep(1) # see how short of a sleep period works
 
