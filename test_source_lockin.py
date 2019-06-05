82a26011cec8602d629d4d8187a3b633ad41e089
barlowmj <barlowmj@umich.edu>
2019-06-05 15:55:16 -0400

    update

--- test_source_lockin.py
+++ test_source_lockin.py
@@ -15,18 +15,17 @@ source.write('SOUR:WAVE:PMAR:OLIN 1')
 lockin.write('PHAS 0.00')
 lockin.write('OFLT 6')
 time.sleep(1)
+source.write('SOUR:WAVE:ARM')
+source.write('SOUR:WAVE:INIT')
 
 amp = .0004
 for i in range(4):
     source.write(f'SOUR:WAVE:AMPL {amp}') # see if this works?
-    source.write('SOUR:WAVE:ARM')
-    source.write('SOUR:WAVE:INIT')
     time.sleep(0.1) # mess with time constant to see if this can be reduced
     vals = lockin.query_ascii_values('SNAP? 1,2')
     volt = sqrt(vals[0]**2 + vals[1]**2)
     res = volt / amp
     print(res)
-    source.write('SOUR:WAVE:ABOR') # does it need to be aborted each time or would changing the amplitude be sufficient? test this
     amp += 0.0002
     time.sleep(1) # see how short of a sleep period works
 
