de849c9755feca741352bbcdf85db667185aa54f
barlowmj <barlowmj@umich.edu>
2019-06-05 17:59:34 -0400

    buggy

--- test_source_lockin.py
+++ test_source_lockin.py
@@ -1,5 +1,5 @@
 import visa
-from numpy import sqrt, logspace
+from numpy import sqrt, logspace, log10, zeros
 import time
 from pylab import plot, show, figure, xlabel, ylabel, title
 
@@ -46,67 +46,104 @@ def time_const(f):
         return 19
 
 def wait_time(tau):
-if tau == 0:
-    return 5*10e-6
-elif tau == 1:
-    return 5*30e-6
-elif tau == 2:
-    return 5*10e-5
-elif tau == 3:
-    return 5*30e-5
-elif tau == 4:
-    return 5*10e-4
-elif tau == 5:
-    return 5*30e-4
-elif tau == 6:
-    return 5*10e-3
-elif tau == 7:
-    return 5*30e-3
-elif tau == 8:
-    return 5*10e-2
-elif tau == 9:
-    return 5*30e-2
-elif tau == 10:
-    return 5*10e-1
-elif tau == 11:
-    return 5*30e-1
-elif tau == 12:
-    return 5*10
-elif tau == 13:
-    return 5*30
-elif tau == 14:
-    return 5*10e1:
-elif tau == 15:
-    return 5*30e1
-elif tau == 16:
-    return 5*10e2
-elif tau == 17:
-    return 5*30e2
-elif tau == 18:
-    return 5*10e3
-elif tau == 19:
-    return 5*30e3
+    if tau == 0:
+        return 5*10e-6
+    elif tau == 1:
+        return 5*30e-6
+    elif tau == 2:
+        return 5*10e-5
+    elif tau == 3:
+        return 5*30e-5
+    elif tau == 4:
+        return 5*10e-4
+    elif tau == 5:
+        return 5*30e-4
+    elif tau == 6:
+        return 5*10e-3
+    elif tau == 7:
+        return 5*30e-3
+    elif tau == 8:
+        return 5*10e-2
+    elif tau == 9:
+        return 5*30e-2
+    elif tau == 10:
+        return 5*10e-1
+    elif tau == 11:
+        return 5*30e-1
+    elif tau == 12:
+        return 5*10
+    elif tau == 13:
+        return 5*30
+    elif tau == 14:
+        return 5*10e1
+    elif tau == 15:
+        return 5*30e1
+    elif tau == 16:
+        return 5*10e2
+    elif tau == 17:
+        return 5*30e2
+    elif tau == 18:
+        return 5*10e3
+    elif tau == 19:
+        return 5*30e3
 
 rm = visa.ResourceManager()
 source = rm.open_resource('GPIB0::7::INSTR')
 lockin = rm.open_resource('GPIB0::5::INSTR')
 
-'''
+# freq_values = logspace(0.1, 100, num=20)
+freq_values = [10, 50, 100, 500, 1000]
+Vx_values = zeros(5)
+Vy_values = zeros(5)
+
 source.write('SOUR:WAVE:FUNC SIN')
-source.write('SOUR:WAVE:FREQ 500')
+source.write('SOUR:WAVE:AMPL 0.001')
 source.write('SOUR:WAVE:PMAR:STAT ON')
 source.write('SOUR:WAVE:PMAR:OLIN 1')
+source.write(f'SOUR:WAVE:FREQ {freq_values[0]}')
 lockin.write('PHAS 0.00')
-lockin.write('OFLT 6')
-time.sleep(1)
 source.write('SOUR:WAVE:ARM')
 source.write('SOUR:WAVE:INIT')
 
-amp_vals = []
-volt_vals = []
-res_vals = []
-amp = 0.0004
-for i in range(20):
+for i in range(len(freq_values)):
+    tau = time_const(freq_values[i])
+    t = wait_time(tau)
+    source.write(f'SOUR:WAVE:FREQ {freq_values[i]}')
+    lockin.write(f'OFLT {tau}')
+    time.sleep(t)
+    vals = lockin.query_ascii_values('SNAP? 1,2')
+    Vx = vals[0]
+    Vy = vals[1]
+    Vx_values[i] = Vx
+    Vy_values[i] = Vy
+
+source.write('SOUR:WAVE:ABOR')
+res_x = Vx_values / 0.001
+res_y = Vy_values / 0.001
+cond_x = 1 / res_x
+cond_y = 1 / res_y
+
+figure(1)
+plot(log10(freq_values), cond_x)
+plot(log10(freq_values), cond_y)
+show()
+
+'''
+    source.write('SOUR:WAVE:FUNC SIN')
+    source.write('SOUR:WAVE:FREQ 500')
+    source.write('SOUR:WAVE:PMAR:STAT ON')
+    source.write('SOUR:WAVE:PMAR:OLIN 1')
+    lockin.write('PHAS 0.00')
+    lockin.write('OFLT 6')
+    time.sleep(1)
+    source.write('SOUR:WAVE:ARM')
+    source.write('SOUR:WAVE:INIT')
+    
+    amp_vals = []
+    volt_vals = []
+    res_vals = []
+    amp = 0.0004
+    for i in range(20):
     amp_vals.append(amp)
     source.write(f'SOUR:WAVE:AMPL {amp}') # see if this works?
     time.sleep(0.075) # mess with time constant to see if this can be reduced
@@ -117,50 +154,24 @@ for i in range(20):
     res_vals.append(res)
     amp += 0.00002
     time.sleep(1)
-
-source.write('SOUR:WAVE:ABOR')
-
-avg_res = sum(res_vals) / len(res_vals)
-figure(1)
-plot(amp_vals, res_vals)
-xlabel('current')
-ylabel('resitance')
-title('resitance vs. current amplitude')
-
-figure(2)
-plot(amp_vals, volt_vals)
-xlabel('current')
-ylabel('voltage')
-title('voltage vs current')
-
-show()
-'''
-
-freq_values = logspace(10e-3, 10e3, num=20)
-Vx_values = []
-Vy_values = []
-
-source.write('SOUR:WAVE:FUNC SIN')
-source.write('SOUR:WAVE:AMPL 0.5')
-source.write('SOUR:WAVE:PMAR:STAT ON')
-source.write('SOUR:WAVE:PMAR:OLIN 1')
-source.write(f'SOUR:WAVE:FREQ {freq_values[-1]}')
-lockin.write('PHAS 0.00')
-time.sleep(1)
-source.write('SOUR:WAVE:ARM')
-source.write('SOUR:WAVE:INIT')
-
-for f in freq_values:
-    tau = time_const(f)
-    t = wait_time(tau)
-    source.write(f'SOUR:WAVE:FREQ {f}')
-    lockin.write(f'OFLT {tau}')
-    time.sleep(t)
-    vals = lockin.query_ascii_values('SNAP? 1,2')
-    Vx = vals[0]
-    Vy = vals[1]
-
-
+    
+    source.write('SOUR:WAVE:ABOR')
+    
+    avg_res = sum(res_vals) / len(res_vals)
+    figure(1)
+    plot(amp_vals, res_vals)
+    xlabel('current')
+    ylabel('resitance')
+    title('resitance vs. current amplitude')
+    
+    figure(2)
+    plot(amp_vals, volt_vals)
+    xlabel('current')
+    ylabel('voltage')
+    title('voltage vs current')
+    
+    show()
+    '''
 
 
 
