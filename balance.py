13908ecdbe678d38eee3cbb34d683b405e856e2a
barlowmj <barlowmj@umich.edu>
2019-06-28 23:26:22 -0400

    with serial stuff pt 1

--- balance.py
+++ balance.py
@@ -1,70 +1,24 @@
 # Program for automatically balancing the capacitance bridge to obtain Vs for null voltage at balance point (or obtain list of Vs values that fit the same requirement)
 
 import time
-import sh
 import visa
-from numpy import sqrt
+import serial
+from check_length import check_length
 
-acc = 1e-6 # set accuracy req
-Vs_values = [] # make empty list to store Vs values? decide if this is the way to output the data or if txt file is better
-gpp = sh.Command('g++') # call gpp('filename.c/pp', '-o', 'filename'), then call Command('./filename') to run the C/++ program that controls the board
 rm = visa.ResourceManager()
-lockin = rm.open_resource('GPIB0::5::INSTR') # should be lock-in amp's address, always check though – also commands to reset it?
+lockin = rm.open_resource('GPIB0::5::INSTR') # should be lock-in amp's address, always check though
 
+clock = 300e3 # clock frequency in Hz
+n = 48 # bits of FTW
+output = 100e3 # desired output frequency in Hz
 
-# need to initialize amplitude of Vs (run c/cpp file through sh)
+ftw = int(output * (2**n / clock)) # calculate frequency tuning word
+ftw_hex_str = string(hex(ftw_hex)) # convert to hex string
+ftw_hex_str = check_length(ftw_hex_str[2:-1]) #eliminate 0x at beginning, make into two's complement hex string
 
-Vs0 = 25 # come up with better numbers when boards arrive and can test, also see about units
+#Vnode_vals0 = lockin.query_ascii_values('SNAP? 1,2') obtain x and y values from lockin
 
-# obtain node voltage from lockin (after delay – see how long)
+board = serial.Serial('/dev/cu.wchusbserial145410') # for my USB input ONLY; figure out how to generalize
+frequency_command = '$WR02' +
 
-time.sleep(1) # set to 1 second for now, probably insufficient – look into time constant to see if settle time gets better
-
-# time constant: increasing makes output more steady, but also takes 5 time constants to settle, have to determine st trade-off is good
-
-Vnode_vals0 = lockin.query_ascii_values('SNAP? 1,2')
-Vnode0 = sqrt(Vnode_vals0[0]**2 + Vnode_vals0[1]**2)
-
-# repeat for slightly lower Vs to obtain next data point for loop
-
-# set voltage amplitude (sh)
-
-Vs1 = 20 # ditto comment from Vs0
-
-# obtain node voltage from lockin
-
-time.sleep(1)
-
-Vnode_vals1 = lockin.query_ascii_values('SNAP? 1,2')
-Vnode1 = sqrt(Vnode_vals1[0]**2 + Vnode_vals1[1]**2)
-
-# set next Vs from Newton's method algorithm
-
-Vs2 = Vs1 - Vnode1*(Vs1 - Vs0)/(Vnode1 - Vnode0)
-
-# obtain node voltage from lockin
-
-time.sleep(1)
-
-
-
-while abs(Vnode2) > acc:
-    # reset parameters
-    Vnode0 = Vnode1
-    Vnode1 = Vnode2
-    Vs0 = Vs1
-    Vs1 = Vs2
-    # Newton's method
-    Vs2 = Vs1 - Vnode1*(Vs1 - Vs0)/(Vnode1 - Vnode0)
-    # delay
-    time.sleep(1)
-    # obtain node voltage, at end runs accuracy check
-    Vnode_vals2 = lockin.query_ascii_values('SNAP? 1,2')
-    Vnode2 = sqrt(Vnode_vals2[0]**2 + Vnode_vals2[0]**2)
-
-# add Vs to a list? or export to file?
-
-Vs_list.append(Vs2)
-
-# should repeat this process to obtain and output Vs values, can then process to determine Cx from known Cs, Ve amplitudes
 
---
+++ check_length.py
@@ -0,0 +1,5 @@
+def check_length(str):
+    if len(str) % 2 == 1:
+        return '0' + str
+    else:
+        return
